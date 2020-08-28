e60a99e9a9f7e08e071d97940719d7550c17d84b
sing-fung <1587898817@qq.com>
2020-08-28 13:54:12 -0400

    batch insert orders

--- src/main/java/com/singfung/blackfriday/controller/OrderController.java
+++ src/main/java/com/singfung/blackfriday/controller/OrderController.java
@@ -6,13 +6,13 @@ import java.util.Map;
 
 import com.singfung.blackfriday.common.Result;
 import com.singfung.blackfriday.model.Order;
-import com.singfung.blackfriday.model.Stock;
 import com.singfung.blackfriday.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.sql.Timestamp;
 
 @Controller
 @RequestMapping("/blackfriday")
@@ -36,6 +36,12 @@ public class OrderController
 	@ResponseBody
 	public ResponseEntity<Result<Object>> add(@RequestBody List<Order> orders)
 	{
+		for(Order order : orders)
+		{
+			Timestamp currTime = new Timestamp(System.currentTimeMillis());
+			order.setOrderTime(currTime);
+		}
+
 		orderService.batchSave(orders);
 		return ResponseEntity.status(HttpStatus.OK).body(Result.success("success"));
 	}
