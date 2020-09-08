713b2d91d5482bbbe80a920a621aa2806386b2cf
sing-fung <1587898817@qq.com>
2020-09-07 22:55:59 -0400

    Replace Jedis with RedisTemplate

--- src/test/java/com/singfung/blackfriday/CyberBlackFridayApplicationTests.java
+++ src/test/java/com/singfung/blackfriday/CyberBlackFridayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineBlackFridayApplicationTests {
+class CyberBlackFridayApplicationTests {
 
     @Test
     void contextLoads() {
